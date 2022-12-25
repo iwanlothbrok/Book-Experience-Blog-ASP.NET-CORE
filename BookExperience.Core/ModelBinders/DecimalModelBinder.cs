49b5755d9d95c6af9e922a462e8631b8802114b6
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-12-25 17:36:41 +0200

    model binders and facebook login were added

---
+++ BookExperience.Core/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,40 @@
+﻿namespace BookExperience.Core.ModelBinders
+{
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using System.Globalization;
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext
+                             .ValueProvider
+                             .GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None && String.IsNullOrWhiteSpace(valueResult.FirstValue) == false)
+            {
+                decimal actualValue = 0;
+                bool succsess = false;
+
+                try
+                {
+                    string decimalValue = valueResult.FirstValue;
+                    decimalValue = decimalValue.Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+                    decimalValue = decimalValue.Replace(".", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+
+                    actualValue = Convert.ToDecimal(decimalValue, CultureInfo.CurrentCulture);
+                    succsess = true;
+                }
+                catch (FormatException fe)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, fe, bindingContext.ModelMetadata);
+                }
+
+                if (succsess)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(actualValue);
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
--- BookExperience.Web/Program.cs
+++ BookExperience.Web/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using BookExperience.Infrastrucutre.Data;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using BookExperience.Core.Extensions;
+using BookExperience.Core.ModelBinders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +13,33 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>
+    (options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
 builder.Services.AddRazorPages();
+
+builder.Services.AddAuthentication()
+    .AddFacebook(options =>
+    {
+        options.AppId = builder.Configuration.GetValue<string>("Facebook:AppId");
+        options.AppSecret = builder.Configuration.GetValue<string>("Facebook:AppSecret");
+    });
+
+builder.Services.AddControllersWithViews()
+    .AddMvcOptions(options =>
+    {
+        options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+        options.ModelBinderProviders.Insert(1, new BookExperience.Core.ModelBinders.DateTimeModelBinderProvider("dd.mm.yyyy"));
+        options.ModelBinderProviders.Insert(2, new DoubleModelBinderProvider());
+    });
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
+app.PrepareDatabase();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -35,6 +59,17 @@ app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "Areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+    endpoints.MapDefaultControllerRoute();
+    endpoints.MapRazorPages();
+    app.MapRazorPages();
+});
 
 app.MapRazorPages();
 
