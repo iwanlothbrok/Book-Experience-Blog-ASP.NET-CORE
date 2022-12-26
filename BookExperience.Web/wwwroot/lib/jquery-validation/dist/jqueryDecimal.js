c39589089dcb8737b47ed17123bd369d21848790
Iwan <99279234+iwanlothbrok@users.noreply.github.com>
2022-12-26 16:18:30 +0200

    adminlte added, layout page and index page were changed

---
+++ BookExperience.Web/wwwroot/lib/jquery-validation/dist/jqueryDecimal.js
@@ -0,0 +1,6 @@
+﻿jQuery(function($) {
+    $.validator.addMethod('number', function (value, element)
+    {
+        return this.optional(element) || /^(?:-?\d+)(?:(\.|,)\d+)?$/.test(value);
+    })
+});
